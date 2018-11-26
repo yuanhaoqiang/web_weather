077cab23fc16d1ace32f1c194acaf65e6fdad12f
yuan <44171469+yuanhaoqiang@users.noreply.github.com>
2018-11-26 22:01:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,230 @@
+<?php
+/**
+  * wechat php test
+  */
+
+//define your token
+define("TOKEN", "yuan");
+$wechatObj = new wechatCallbackapiTest();
+$wechatObj->responseMsg();
+//$wechatObj->valid();
+
+class wechatCallbackapiTest
+{
+    /*public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }*/
+
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+          //extract post data
+        if (!empty($postStr)){
+                
+                  $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $RX_TYPE = trim($postObj->MsgType);
+
+                switch($RX_TYPE)
+                {
+                    case "text":
+                        $resultStr = $this->handleText($postObj);
+                        break;
+                    case "event":
+                        $resultStr = $this->handleEvent($postObj);
+                        break;
+                    default:
+                        $resultStr = "Unknow msg type: ".$RX_TYPE;
+                        break;
+                }
+                echo $resultStr;
+        }else {
+            echo "";
+            exit;
+        }
+    }
+
+    public function handleText($postObj)
+    {
+        $fromUsername = $postObj->FromUserName;
+        $toUsername = $postObj->ToUserName;
+        $keyword = trim($postObj->Content);
+        $time = time();
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>0</FuncFlag>
+                    </xml>";             
+        if(!empty( $keyword ))
+        {
+            $msgType = "text";
+
+            //天气
+            $str = mb_substr($keyword,-2,2,"UTF-8");
+            $str_key = mb_substr($keyword,0,-2,"UTF-8");
+            if($str == '天气' && !empty($str_key)){
+                $data = $this->weather($str_key);
+              
+             //   if(empty($data->weatherinfo)){
+             //       $contentStr = "抱歉，没有查到\"".$str_key."\"的天气信息！".$c_name;
+             //   } else {
+//                    $contentStr = "【".$data->weatherinfo->city."天气预报】\n".$data->weatherinfo->date_y." ".$data->weatherinfo->fchh."时发布"."\n\n实时天气\n".$data->weatherinfo->weather1." ".$data->weatherinfo->temp1." ".$data->weatherinfo->wind1."\n\n温馨提示：".$data->weatherinfo->index_d."\n\n明天\n".$data->weatherinfo->weather2." ".$data->weatherinfo->temp2." ".$data->weatherinfo->wind2."\n\n后天\n".$data->weatherinfo->weather3." ".$data->weatherinfo->temp3." ".$data->weatherinfo->wind3;
+  					$contentStr = "【".$data->weatherinfo->city."天气预报】\n"."2018年12月12日发布"."\n\n实时天气\n"."晴"." "."5℃-8℃ "."南风3到4级"."\n\n";
+              //  }
+            } else {
+                $contentStr = "感谢您的关注"."\n"."\n"."目前平台功能如下："."\n"."【1】 查天气，如输入：北京天气"."\n"."更多内容，敬请期待...";
+            }
+            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+            echo $resultStr;	
+        }else{
+            echo "Input something...";
+        }
+    }
+
+    public function handleEvent($object)
+    {
+        $contentStr = "";
+        switch ($object->Event)
+        {
+            case "subscribe":
+                $contentStr = "感谢您关注【卓锦苏州】"."\n"."微信号：zhuojinsz"."\n"."卓越锦绣，名城苏州，我们为您提供苏州本地生活指南，苏州相关信息查询，做最好的苏州微信平台。"."\n"."目前平台功能如下："."\n"."【1】 查天气，如输入：苏州天气"."\n"."【2】 查公交，如输入：苏州公交178"."\n"."【3】 翻译，如输入：翻译I love you"."\n"."【4】 苏州信息查询，如输入：苏州观前街"."\n"."更多内容，敬请期待...";
+                break;
+            default :
+                $contentStr = "Unknow Event: ".$object->Event;
+                break;
+        }
+        $resultStr = $this->responseText($object, $contentStr);
+        return $resultStr;
+    }
+    
+    public function responseText($object, $content, $flag=0)
+    {
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>%d</FuncFlag>
+                    </xml>";
+        $resultStr = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content, $flag);
+        return $resultStr;
+    }
+
+    private function weather($n){
+        include("weather_cityId.php");
+     	
+        $c_name=$weather_cityId[$n];
+      
+        if(!empty($c_name)){
+            $json=file_get_contents("http://m.weather.com.cn/data/".$c_name.".html");
+            return json_decode($json);
+        } else {
+            return null;
+        }
+    }
+
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];    
+                
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+
+
+
+
+/*    //获得参数 signature nonce token timestamp echostr
+    $nonce     = $_GET['nonce'];
+    $token     = 'yuan';
+    $timestamp = $_GET['timestamp'];
+    $echostr   = $_GET['echostr'];
+    $signature = $_GET['signature'];
+    //形成数组，然后按字典序排序
+    $array = array();
+    $array = array($nonce, $timestamp, $token);
+    sort($array);
+    //拼接成字符串,sha1加密 ，然后与signature进行校验
+    $str = sha1( implode( $array ) );
+    if( $str == $signature && $echostr ){
+        //第一次接入weixin api接口的时候
+        echo  $echostr;
+        exit;
+    }else{
+
+        //1.获取到微信推送过来post数据（xml格式）
+        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        //2.处理消息类型，并设置回复类型和内容
+        /*<xml>
+<ToUserName><![CDATA[toUser]]></ToUserName>
+<FromUserName><![CDATA[FromUser]]></FromUserName>
+<CreateTime>123456789</CreateTime>
+<MsgType><![CDATA[event]]></MsgType>
+<Event><![CDATA[subscribe]]></Event>
+</xml>*/
+
+  /*      $postObj = simplexml_load_string( $postArr );
+        //$postObj->ToUserName = '';
+        //$postObj->FromUserName = '';
+        //$postObj->CreateTime = '';
+        //$postObj->MsgType = '';
+        //$postObj->Event = '';
+        // gh_e79a177814ed
+        //判断该数据包是否是订阅的事件推送
+        if( strtolower( $postObj->MsgType) == 'event'){
+            //如果是关注 subscribe 事件
+            if( strtolower($postObj->Event == 'subscribe') ){
+                //回复用户消息(纯文本格式)
+                $toUser   = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $time     = time();
+                $msgType  =  'text';
+                $content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
+                $template = "<xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Content><![CDATA[%s]]></Content>
+                                </xml>";
+                $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                echo $info;
+                /*<xml>
+                <ToUserName><![CDATA[toUser]]></ToUserName>
+                <FromUserName><![CDATA[fromUser]]></FromUserName>
+                <CreateTime>12345678</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[你好]]></Content>
+                </xml>*/
+
+/*
+            }
+        }
+
+    }
+*/
